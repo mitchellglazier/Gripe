f2da068b8454f92fca4d9f752e3426e53429c4a2
Mitchell Glazier <glazierm@gmail.com>
2019-02-05 16:35:54 -0700

    added validations

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,9 +3,16 @@ import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styles: [ `
+        em { float:right; color:#F05C65; padding-left:10px; }
+    `]
 })
 export class LoginComponent {
+    username
+    password
+    mouseoverLogin
+
     constructor(private authService: AuthService, private router: Router) {
     }
 
--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,18 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  template: `
-    <h1>Edit Your Profile</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Edit profile form will go here]</h3>
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default">Cancel</button>
-    </div>
-  `,
+  templateUrl: '.profile.component.html'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  ngOnInit() {
+    
+  }
 
 }
